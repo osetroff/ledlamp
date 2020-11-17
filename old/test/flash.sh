50358a1e06c44a84e93f1309a9bb11907841776f
osetroff <osetroff@mail.ru>
2020-11-17 20:18:53 +0400

    ...

--- old/test/flash.sh
+++
@@ -1,2 +0,0 @@
-#avrdude -v -v -p atmega328p -c avrispmkii -U flash:w:main.hex:i -F -P /dev/ttyUSB0
-avrdude -v -D -patmega328p -carduino "-P/dev/ttyUSB0" -b38400 "-Uflash:w:main.hex:i"
--- old/test/m.sh
+++
@@ -1,4 +0,0 @@
-avr-g++ -v -Wall -g -Os -mmcu=atmega328p -o main.bin main.cpp
-avr-size -C main.bin
-avr-objcopy -j .text -j .data -O ihex main.bin main.hex
-
